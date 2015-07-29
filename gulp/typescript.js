2df70eb2469c669221bcf692b34a4ff7884029fb
Jason Young <jason@ytechie.com>
2015-07-29 16:53:03 -0500

    Added typescript source mapping files

--- gulp/typescript.js
+++ gulp/typescript.js
@@ -2,14 +2,19 @@
 
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
+var sourcemaps = require('gulp-sourcemaps');
 
 module.exports = function(options) {
   gulp.task('typescript', function () {
     var tsResult = gulp.src('src/**/*.ts')
+      .pipe(sourcemaps.init())
       .pipe(ts({
           noImplicitAny: false,
           target: 'ES5'
         }));
-    return tsResult.js.pipe(gulp.dest('src'));
+    return tsResult.js
+      .pipe(sourcemaps.write({includeContent:false, sourceRoot: '/'}))
+      .pipe(gulp.dest('src'))
+      ;
   });
 }

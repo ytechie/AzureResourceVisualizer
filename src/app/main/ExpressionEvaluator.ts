2b3fb98e33571f67d8b48e6aacbeebba74c1fe02
Jason Young <jason@ytechie.com>
2015-07-29 17:18:07 -0500

    Stubbing expression evaluator

---
+++ src/app/main/ExpressionEvaluator.ts
@@ -0,0 +1,14 @@
+/// <reference path="ArmTemplate.ts" />
+/// <reference path="ExpressionParser.ts" />
+
+class ExpressionEvaluator{
+	private armTemplate:ArmTemplateInterface;
+	
+	constructor(armTemplate:ArmTemplateInterface) {
+		this.armTemplate = armTemplate;
+	}
+	
+	evaluateExpression(expression:Expression) {
+		
+	}
+}

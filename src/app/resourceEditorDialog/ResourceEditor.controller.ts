46adc3a7789dc30a9e8079c195b9dacab883b498
Jason Young <jason@ytechie.com>
2015-07-31 12:09:43 -0500

    Added resource validation button

--- src/app/resourceEditorDialog/ResourceEditor.controller.ts
+++ src/app/resourceEditorDialog/ResourceEditor.controller.ts
@@ -5,25 +5,34 @@ angular.module('vis').controller('ResourceEditorController', function ($scope, $
 	$scope.resource = resource;
   $scope.resourceJson = JSON.stringify(resource, null, 2);
   
+  $scope.validate = function() {
+    try {
+       JSON.parse($scope.resourceJson);
+       $scope.validationResult = "Valid JSON!";
+    } catch(err) {
+      $scope.validationResult = "Invalid JSON: " + err.toString();
+    }
+  }
+  
   $scope.save = function () {
     var newResource:Resource;
     
     try {
       newResource = JSON.parse($scope.resourceJson);
-      
-      //Update the EXISTING resource without destroying it
-      for(var key in resource) {
-        if(resource.hasOwnProperty(key)) {
-          delete resource[key];
-        }
-      }
-      
-      $.extend(resource, newResource);
     } catch(err) {
       alert('Invalid JSON: ' + err.toString());
       
       return;
     }
+    
+    //Update the EXISTING resource without destroying it
+    for(var key in resource) {
+      if(resource.hasOwnProperty(key)) {
+        delete resource[key];
+      }
+    }
+    
+    $.extend(resource, newResource);
       
 	  $modalInstance.close(resource);
 	};

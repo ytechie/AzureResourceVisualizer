aa63b624741ef9e68fcc81a55b7bdb106e4c5440
kwittkopf <kwittkopf@hotmail.com>
2015-07-31 12:05:40 -0700

    Added additional resource definitions

--- src/app/main/ToolboxITems.ts
+++ src/app/main/ToolboxITems.ts
@@ -6,13 +6,13 @@
         "Virtual Machine",
         'Microsoft.Compute/virtualMachines' ),
       new ToolboxResource(
-        "Azure load balancer.png",
-        "Load Balancer",
-        'Microsoft.Network/loadBalancers' ),
-      new ToolboxResource(
         "Availability Set.png",
         "Availability Set",
         'Microsoft.Compute/availabilitySets' ),
+      new ToolboxResource(
+        "Azure load balancer.png",
+        "Load Balancer",
+        'Microsoft.Network/loadBalancers' ),
       new ToolboxResource(
         "Virtual Network.png",
         "VPN",
@@ -26,7 +26,27 @@
         "Public IP",
         'Microsoft.Network/publicIPAddresses' ),
       new ToolboxResource(
+        "Unidentified feature object.png",
+        "NSG",
+        'Microsoft.Network/networkSecurityGroups' ),
+      new ToolboxResource(
+        "Unidentified feature object.png",
+        "App Gateway",
+        'Microsoft.Network/applicationGateways' ),
+      new ToolboxResource(
         "Storage (Azure).png",
         "Storage Acct",
-        'Microsoft.Storage/storageAccounts' )
+        'Microsoft.Storage/storageAccounts' ),
+      new ToolboxResource(
+        "Unidentified feature object.png",
+        "Automation",
+        'Microsoft.Automation/automationAccounts' ),
+      new ToolboxResource(
+        "Logic App.png",
+        "Workflow",
+        'Microsoft.Logic/workflows' ),
+      new ToolboxResource(
+        "Web App (was Websites).png",
+        "Server Farm",
+        'Microsoft.Web/serverfarms' )
     ];  

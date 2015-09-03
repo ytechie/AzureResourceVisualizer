5102df04d68707f463587fa17916bd7e88129944
Jason Young <jason@ytechie.com>
2015-09-02 19:34:49 -0500

    Newer version of azure deploy script

--- deploy.cmd
+++ deploy.cmd
@@ -2,15 +2,15 @@
 
 :: ----------------------
 :: KUDU Deployment Script
-:: Version: 0.1.10
+:: Version: 0.2.2
 :: ----------------------
 
 :: Prerequisites
 :: -------------
 
 :: Verify node.js installed
-where node 2>nul >nul  
-IF %ERRORLEVEL% NEQ 0 (  
+where node 2>nul >nul
+IF %ERRORLEVEL% NEQ 0 (
   echo Missing node.js executable, please install node.js, if already installed make sure it can be reached from current environment.
   goto error
 )
@@ -22,15 +22,15 @@ setlocal enabledelayedexpansion
 
 SET ARTIFACTS=%~dp0%..\artifacts
 
-IF NOT DEFINED DEPLOYMENT_SOURCE (  
+IF NOT DEFINED DEPLOYMENT_SOURCE (
   SET DEPLOYMENT_SOURCE=%~dp0%.
 )
 
-IF NOT DEFINED DEPLOYMENT_TARGET (  
+IF NOT DEFINED DEPLOYMENT_TARGET (
   SET DEPLOYMENT_TARGET=%ARTIFACTS%\wwwroot
 )
 
-IF NOT DEFINED NEXT_MANIFEST_PATH (  
+IF NOT DEFINED NEXT_MANIFEST_PATH (
   SET NEXT_MANIFEST_PATH=%ARTIFACTS%\manifest
 
   IF NOT DEFINED PREVIOUS_MANIFEST_PATH (
@@ -38,7 +38,7 @@ IF NOT DEFINED NEXT_MANIFEST_PATH (
   )
 )
 
-IF NOT DEFINED KUDU_SYNC_CMD (  
+IF NOT DEFINED KUDU_SYNC_CMD (
   :: Install kudu sync
   echo Installing Kudu Sync
   call npm install kudusync -g --silent
@@ -47,79 +47,104 @@ IF NOT DEFINED KUDU_SYNC_CMD (
   :: Locally just running "kuduSync" would also work
   SET KUDU_SYNC_CMD=%appdata%\npm\kuduSync.cmd
 )
-IF NOT DEFINED DEPLOYMENT_TEMP (  
-  SET DEPLOYMENT_TEMP=%temp%\___deployTemp%random%
-  SET CLEAN_LOCAL_DEPLOYMENT_TEMP=true
+goto Deployment
+
+:: Utility Functions
+:: -----------------
+
+:SelectNodeVersion
+
+IF DEFINED KUDU_SELECT_NODE_VERSION_CMD (
+  :: The following are done only on Windows Azure Websites environment
+  call %KUDU_SELECT_NODE_VERSION_CMD% "%DEPLOYMENT_SOURCE%" "%DEPLOYMENT_TARGET%" "%DEPLOYMENT_TEMP%"
+  IF !ERRORLEVEL! NEQ 0 goto error
+
+  IF EXIST "%DEPLOYMENT_TEMP%\__nodeVersion.tmp" (
+    SET /p NODE_EXE=<"%DEPLOYMENT_TEMP%\__nodeVersion.tmp"
+    IF !ERRORLEVEL! NEQ 0 goto error
+  )
+  
+  IF EXIST "%DEPLOYMENT_TEMP%\__npmVersion.tmp" (
+    SET /p NPM_JS_PATH=<"%DEPLOYMENT_TEMP%\__npmVersion.tmp"
+    IF !ERRORLEVEL! NEQ 0 goto error
+  )
+
+  IF NOT DEFINED NODE_EXE (
+    SET NODE_EXE=node
+  )
+
+  SET NPM_CMD="!NODE_EXE!" "!NPM_JS_PATH!"
+) ELSE (
+  SET NPM_CMD=npm
+  SET NODE_EXE=node
 )
 
-IF DEFINED CLEAN_LOCAL_DEPLOYMENT_TEMP (  
-  IF EXIST "%DEPLOYMENT_TEMP%" rd /s /q "%DEPLOYMENT_TEMP%"
-  mkdir "%DEPLOYMENT_TEMP%"
-)
-
-IF NOT DEFINED MSBUILD_PATH (  
-  SET MSBUILD_PATH=%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
-)
+goto :EOF
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Deployment
 :: ----------
 
-:: Cleanup node_modules if it exists 
-call rmdir node_modules /s/q
+:Deployment
+echo Handling node.js deployment.
 
-:: Restore NPM packages
-echo Installing npm packages: Starting %TIME%
-:: Update NPM
-call npm install npm
-call npm install --production
-echo Installing npm packages: Finished %TIME%
-IF !ERRORLEVEL! NEQ 0 goto error
-
-:: Restore Bower packages
-echo Installing bower packages: Starting %TIME%
-call :ExecuteCmd npm install bower -g
-call :ExecuteCmd bower install
-echo Installing bower packages: Finished %TIME%
-IF !ERRORLEVEL! NEQ 0 goto error
-
-:: Restore Gulp packages and run Gulp tasks
-IF /I "gulpfile.js" NEQ "" (  
-  IF !ERRORLEVEL! NEQ 0 goto error
-  echo Running Gulp deployment: Starting %TIME%
-  call :ExecuteCmd node_modules\.bin\gulp
-  echo Running Gulp deployment: Finished %TIME%
+:: 1. KuduSync
+IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 
-:: KuduSync
-IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (  
-  echo Running Kudu Sync: Starting %TIME%
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%\dist" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
-  echo Running Kudu Sync: Finished %TIME%
+:: 2. Select node version
+call :SelectNodeVersion
+
+:: 3. Install npm packages
+IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
+  pushd "%DEPLOYMENT_TARGET%"
+  call :ExecuteCmd !NPM_CMD! install
   IF !ERRORLEVEL! NEQ 0 goto error
+  popd
 )
+
+
+:: 4. Install bower packages
+IF EXIST "%DEPLOYMENT_TARGET%\bower.json" (
+  pushd "%DEPLOYMENT_TARGET%"
+  call :ExecuteCmd .\node_modules\.bin\bower install
+  IF !ERRORLEVEL! NEQ 0 goto error
+  popd
+)
+
+
+:: 5. Run gulp transformations
+IF EXIST "%DEPLOYMENT_TARGET%\gulpfile.js" (
+  pushd "%DEPLOYMENT_TARGET%"
+  call :ExecuteCmd .\node_modules\.bin\gulp
+  IF !ERRORLEVEL! NEQ 0 goto error
+  popd
+)
+
+
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 :: Post deployment stub
-IF DEFINED POST_DEPLOYMENT_ACTION call "%POST_DEPLOYMENT_ACTION%"  
+IF DEFINED POST_DEPLOYMENT_ACTION call "%POST_DEPLOYMENT_ACTION%"
 IF !ERRORLEVEL! NEQ 0 goto error
 
 goto end
 
 :: Execute command routine that will echo out when error
 :ExecuteCmd
-setlocal  
-set _CMD_=%*  
-call %_CMD_%  
-if "%ERRORLEVEL%" NEQ "0" echo Failed exitCode=%ERRORLEVEL%, command=%_CMD_%  
+setlocal
+set _CMD_=%*
+call %_CMD_%
+if "%ERRORLEVEL%" NEQ "0" echo Failed exitCode=%ERRORLEVEL%, command=%_CMD_%
 exit /b %ERRORLEVEL%
 
 :error
-endlocal  
-echo An error has occurred during web site deployment.  
-call :exitSetErrorLevel  
+endlocal
+echo An error has occurred during web site deployment.
+call :exitSetErrorLevel
 call :exitFromFunction 2>nul
 
 :exitSetErrorLevel
@@ -129,5 +154,5 @@ exit /b 1
 ()
 
 :end
-endlocal  
-echo Finished successfully.  
+endlocal
+echo Finished successfully.

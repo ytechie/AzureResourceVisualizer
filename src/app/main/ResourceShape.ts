c0f311314f5dd536d5c72c0fdd6da26d2fca0d87
Jason Young <jason@ytechie.com>
2015-12-08 09:05:55 -0600

    Better titles for deployments

--- src/app/main/ResourceShape.ts
+++ src/app/main/ResourceShape.ts
@@ -14,23 +14,27 @@ module ArmViz {
             this.sourceResource = resource;
             this.sourceToolboxItem = toolboxItem;
                     
-        if(toolboxItem) {
-                this.titleText = toolboxItem.friendlyName;
-            
-                this.attributes.attrs.image = { 'ref-x': 25, 'ref-y':5, ref: 'rect', width:60, height:60};
-                this.PNGRef = "/assets/toolbox-icons/" + toolboxItem.iconName;
-                this.attributes.attrs.image['xlink:href'] = this.PNGRef;
+            if(toolboxItem) {
+                    this.titleText = toolboxItem.friendlyName;
                 
-                this.attributes.attrs.text = {'ref-dy' :-15, ref: 'rect', 'ref-x':55,
-                    'x-alignment' :'middle', 'text': toolboxItem.friendlyName, 'fill': '#000000'};
-        } else {
-            this.titleText = this.sourceResource.type.replace("/", "\n");
-        }  
-        
-        this.attributes.attrs.rect.fill = '#FFFFFF';
-        this.attributes.attrs.rect.stroke = '#0079D6';
-        this.attributes.attrs.rect['stroke-width'] = 2;
+                    this.attributes.attrs.image = { 'ref-x': 25, 'ref-y':5, ref: 'rect', width:60, height:60};
+                    this.PNGRef = "/assets/toolbox-icons/" + toolboxItem.iconName;
+                    this.attributes.attrs.image['xlink:href'] = this.PNGRef;
+                    
+                    this.attributes.attrs.text = {'ref-dy' :-15, ref: 'rect', 'ref-x':55,
+                        'x-alignment' :'middle', 'text': toolboxItem.friendlyName, 'fill': '#000000'};
+            } else {
+                this.titleText = this.sourceResource.type.replace("/", "\n");
+            }
             
+            //Override the title if it's a deployment
+            if(this.sourceResource.type === "Microsoft.Resources/deployments") {
+                this.titleText = this.sourceResource.name;
+            }
+            
+            this.attributes.attrs.rect.fill = '#FFFFFF';
+            this.attributes.attrs.rect.stroke = '#0079D6';
+            this.attributes.attrs.rect['stroke-width'] = 2;
         }
         
         

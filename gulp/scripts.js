209a631e2a6583b15c80f90a41dadacc0b85c2a8
Jason Young <jason@ytechie.com>
2016-01-07 14:31:25 -0600

    Put ace files in a place to allow 'require' to work

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -16,16 +16,20 @@ var $ = require('gulp-load-plugins')();
   });
 
   gulp.task('scripts', function () {
-  return gulp.src([
-      path.join(conf.paths.src, '/app/**/*.ts'),
-      path.join('!' + conf.paths.src, '/app/**/*.spec.ts')])
-    .pipe($.sourcemaps.init())
-    .pipe($.tslint())
-    .pipe($.tslint.report('prose', { emitError: false }))
-    .pipe($.typescript(tsProject)).on('error', conf.errorHandler('TypeScript'))
-    .pipe($.concat('index.module.js'))
-    //.pipe($.sourcemaps.write())
-    .pipe(gulp.dest(path.join(conf.paths.tmp, '/serve/app')))
-    .pipe(browserSync.reload({ stream: true }))
-    .pipe($.size())
+    //Not a fan of putting these in the root, but it's to get the ace require to work
+    gulp.src(path.join('bower_components', '/ace-builds/src-noconflict/**/*json*.*'))
+        .pipe(gulp.dest(path.join(conf.paths.dist, '/')));
+        
+    return gulp.src([
+        path.join(conf.paths.src, '/app/**/*.ts'),
+        path.join('!' + conf.paths.src, '/app/**/*.spec.ts')])
+        .pipe($.sourcemaps.init())
+        .pipe($.tslint())
+        .pipe($.tslint.report('prose', { emitError: false }))
+        .pipe($.typescript(tsProject)).on('error', conf.errorHandler('TypeScript'))
+        .pipe($.concat('index.module.js'))
+        //.pipe($.sourcemaps.write())
+        .pipe(gulp.dest(path.join(conf.paths.tmp, '/serve/app')))
+        .pipe(browserSync.reload({ stream: true }))
+        .pipe($.size())
 });

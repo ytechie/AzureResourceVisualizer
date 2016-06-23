126dd7bcdd721a510ae75b8d820852cc45bdf9ac
Shenglong Li <shenglong.li@outlook.com>
2016-06-23 09:32:34 -0700

    ExpressionParser refactoring

--- src/app/main/ExpressionParser.spec.js
+++ src/app/main/ExpressionParser.spec.js
@@ -12,6 +12,19 @@ describe("ExpressionParser", function () {
     expect(exp.operands[0]).toEqual('var1');
   });
 
+  it('should parse simple variable expression with properties', function () {
+    var ep = new ArmViz.ExpressionParser();
+    var exp = ep.parse("variables('var1').var2.var3.var4")
+
+    expect(exp.operator).toEqual("variables");
+    expect(exp.operands.length).toEqual(1);
+    expect(exp.operands[0]).toEqual('var1');
+    expect(exp.properties.length).toEqual(3);
+    expect(exp.properties[0]).toEqual('var2');
+    expect(exp.properties[1]).toEqual('var3');
+    expect(exp.properties[2]).toEqual('var4');
+  });
+
   it('should parse string', function () {
     var ep = new ArmViz.ExpressionParser();
     var exp = ep.parse("storageLoop")
@@ -19,6 +32,15 @@ describe("ExpressionParser", function () {
     expect(exp.operator).toEqual("");
     expect(exp.operands.length).toEqual(1);
     expect(exp.operands[0]).toEqual('storageLoop');
+  });
+
+  it ('should parse string with space', function() {
+    var ep = new ArmViz.ExpressionParser();
+    var exp = ep.parse('virtual network');
+
+    expect(exp.operator).toEqual("");
+    expect(exp.operands.length).toEqual(1);
+    expect(exp.operands[0]).toEqual('virtual network');
   });
 
   it('should parse expression with multiple parameters', function () {
@@ -41,6 +63,19 @@ describe("ExpressionParser", function () {
     expect(exp.operands[1]).toEqual('var2');
   });
 
+  it('should parse expression with multiple parameters with properties', function () {
+    var ep = new ArmViz.ExpressionParser();
+    var exp = ep.parse("resourceId('var1', 'var2').var3.var4")
+
+    expect(exp.operator).toEqual("resourceId");
+    expect(exp.operands.length).toEqual(2);
+    expect(exp.operands[0]).toEqual('var1');
+    expect(exp.operands[1]).toEqual('var2');
+    expect(exp.properties.length).toEqual(2);
+    expect(exp.properties[0]).toEqual('var3');
+    expect(exp.properties[1]).toEqual('var4');
+  });
+
   it('should parse nested expressions', function () {
     var ep = new ArmViz.ExpressionParser();
     var exp = ep.parse("concat('var1', nest('var2', 'var3'))")
@@ -55,6 +90,70 @@ describe("ExpressionParser", function () {
     expect(nested.operands.length).toEqual(2);
     expect(nested.operands[0]).toEqual('var2');
     expect(nested.operands[1]).toEqual('var3');
+  });
+
+  it('should parse nested expressions with properties', function () {
+    var ep = new ArmViz.ExpressionParser();
+    var exp = ep.parse("concat(nested('var1', var2').var3.var4, 'var5')");
+
+    expect(exp.operator).toEqual("concat");
+    expect(exp.operands.length).toEqual(2);
+    expect(exp.operands[1]).toEqual('var5');
+
+    var nested = exp.operands[0];
+    expect(nested.operator).toEqual('nested');
+    expect(nested.operands.length).toEqual(2);
+    expect(nested.operands[0]).toEqual('var1');
+    expect(nested.operands[1]).toEqual('var2');
+    expect(nested.properties.length).toEqual(2);
+    expect(nested.properties[0]).toEqual('var3');
+    expect(nested.properties[1]).toEqual('var4');
+
+  });
+
+  it('should parse nested expressions with properties', function () {
+    var ep = new ArmViz.ExpressionParser();
+    var exp = ep.parse("concat('var1', nested('var2', var3').var4.var5)");
+
+    expect(exp.operator).toEqual("concat");
+    expect(exp.operands.length).toEqual(2);
+    expect(exp.operands[0]).toEqual('var1');
+
+    var nested = exp.operands[1];
+    expect(nested.operator).toEqual('nested');
+    expect(nested.operands.length).toEqual(2);
+    expect(nested.operands[0]).toEqual('var2');
+    expect(nested.operands[1]).toEqual('var3');
+    expect(nested.properties.length).toEqual(2);
+    expect(nested.properties[0]).toEqual('var4');
+    expect(nested.properties[1]).toEqual('var5');
+
+  });
+
+  it('should parse nested expressions with properties', function () {
+    var ep = new ArmViz.ExpressionParser();
+    var exp = ep.parse("concat(nested1('var11', var12').var13.var14, nested2('var21', 'var22').var23.var24)");
+
+    expect(exp.operator).toEqual("concat");
+    expect(exp.operands.length).toEqual(2);
+
+    var nested1 = exp.operands[0];
+    expect(nested1.operator).toEqual('nested1');
+    expect(nested1.operands.length).toEqual(2);
+    expect(nested1.operands[0]).toEqual('var11');
+    expect(nested1.operands[1]).toEqual('var12');
+    expect(nested1.properties.length).toEqual(2);
+    expect(nested1.properties[0]).toEqual('var13');
+    expect(nested1.properties[1]).toEqual('var14');
+
+    var nested2 = exp.operands[1];
+    expect(nested2.operator).toEqual('nested2');
+    expect(nested2.operands.length).toEqual(2);
+    expect(nested2.operands[0]).toEqual('var21');
+    expect(nested2.operands[1]).toEqual('var22');
+    expect(nested2.properties.length).toEqual(2);
+    expect(nested2.properties[0]).toEqual('var23');
+    expect(nested2.properties[1]).toEqual('var24');
   });
 
   it('should strip brackets', function () {
@@ -77,5 +176,36 @@ describe("ExpressionParser", function () {
     expect(exp.operands[1].operands[0].operator).toEqual('parameters');
     expect(exp.operands[1].operands[0].operands[0]).toEqual('endpointName');
     expect(exp.operands[1].operands[1]).toEqual('gateway');
+  });
+
+  it('should parse multi-nested expressions with properties', function () {
+    var ep = new ArmViz.ExpressionParser();
+    var exp = ep.parse(
+      "variables(resourceId(parameters('endpoint').type, concat('-', parameters('endpoint').name))).default.name");
+
+    expect(exp.operator).toEqual('variables');
+    expect(exp.operands.length).toEqual(1);
+    expect(exp.properties.length).toEqual(2);
+    expect(exp.properties[0]).toEqual('default');
+    expect(exp.properties[1]).toEqual('name');
+
+    var resourceId = exp.operands[0];
+    expect(resourceId.operands.length).toEqual(2);
+
+    var parameters1 = resourceId.operands[0];
+    expect(parameters1.operands.length).toEqual(1);
+    expect(parameters1.operands[0]).toEqual('endpoint');
+    expect(parameters1.properties.length).toEqual(1);
+    expect(parameters1.properties[0]).toEqual('type');
+
+    var concat = resourceId.operands[1];
+    expect(concat.operands.length).toEqual(2);
+    expect(concat.operands[0]).toEqual('-');
+
+    var parameters2 = concat.operands[1];
+    expect(parameters2.operands.length).toEqual(1);
+    expect(parameters2.operands[0]).toEqual('endpoint');
+    expect(parameters2.properties.length).toEqual(1);
+    expect(parameters2.properties[0]).toEqual('name');
   });
 });
--- src/app/main/ExpressionParser.ts
+++ src/app/main/ExpressionParser.ts
@@ -1,69 +1,92 @@
 module ArmViz {
   export class ExpressionParser {
-    parse(expressionText: string): Expression {
-      var buffer = '';
+    parse(source: string): Expression {
+      let pre = '';
+      let buffer = '';
+      let isProperty = false;
+      let exp: Expression = null;
+      let expStack: Expression[] = [];
 
-      var expression: Expression = new Expression(expressionText);
-
-      var expressionStack = new Array<Expression>();
-
-      if (expressionText.charAt(0) === '[' && expressionText.charAt(expressionText.length - 1) === ']') {
-        expressionText = expressionText.substr(1, expressionText.length - 2);
+      // Trim square brackets
+      if (source[0] === '[' && source[source.length - 1] === ']') {
+        source = source.substr(1, source.length - 2);
       }
 
-      //An expression could just be a string
-      if (expressionText.indexOf('(') < 0) {
-        expression.operands.push(expressionText);
-        return expression;
+      // An expression could just be a string
+      if (source.indexOf('(') < 0) {
+        exp = new Expression();
+        exp.operands.push(source);
+        return exp;
       }
 
-      for (var i: number = 0; i < expressionText.length; i++) {
-        var ch: string = expressionText.charAt(i);
-
-        if (ch === "'" && buffer.length === 0) {
-          //ignore start of operand
-        } else if (ch === "'") {
-          //ignore
-        } else if (ch === " ") {
-          //Ignore whitespace here
-        } else if (ch === ",") {
-          //Comma separated string operands
-          if (buffer.length > 0) {
-            expression.operands.push(buffer);
+      // Parse expression
+      for (let ch of source) {
+        switch (ch) {
+          case "'":
+            break;
+          case '(':
+            if (exp) {
+              expStack.push(exp);
+            }
+            exp = new Expression();
+            exp.operator = buffer.trim();
             buffer = '';
-          }
-        } else if (ch === ")") {
-          let outerExpression: Expression;
 
-          if (buffer.length > 0) {
-            expression.operands.push(buffer);
-            buffer = '';
-          }
+            break;
+          case ')':
+            if (buffer.length > 0) {
+              if (isProperty) {
+                let operand = <Expression>exp.operands[exp.operands.length - 1];
+                operand.properties.push.apply(operand.properties, buffer.trim().split('.'));
+                isProperty = false;
+              } else {
+                exp.operands.push(buffer.trim());
+              }
+              buffer = '';
+            }
 
-          if (expression !== expressionStack[0]) {
-            expressionStack.pop();
-            outerExpression = expressionStack[expressionStack.length - 1];
-          }
-          if (outerExpression) {
-            outerExpression.operands.push(expression);
-            expression = outerExpression;
-          }
-        } else if (ch === "(") {
-          if (expressionStack.length === 0) {
-            expressionStack.push(expression);
-          } else {
-            expression = new Expression();
-            expressionStack.push(expression);
-          }
+            if (expStack.length > 0) {
+              let parent = expStack.pop();
+              parent.operands.push(exp);
+              exp = parent;
+            }
 
-          expression.operator = buffer;
-          buffer = '';
-        } else {
-          buffer += ch;
+            break;
+          case '.':
+            if (pre === ')') {
+              isProperty = true;
+            } else {
+              buffer += ch;
+            }
+
+            break;
+          case ',':
+            if (buffer.length > 0) {
+              if (isProperty) {
+                let operand = <Expression>exp.operands[exp.operands.length - 1];
+                operand.properties.push.apply(operand.properties, buffer.trim().split('.'));
+                isProperty = false;
+              } else {
+                exp.operands.push(buffer.trim());
+              }
+              buffer = '';
+            }
+
+            break;
+          default:
+            buffer += ch;
+            break;
         }
+
+        pre = ch;
       }
 
-      return expression;
+      // Post parsing process
+      if (buffer.length > 0 && isProperty) {
+        exp.properties.push.apply(exp.properties, buffer.trim().split('.'));
+      }
+
+      return exp;
     }
   }
 }

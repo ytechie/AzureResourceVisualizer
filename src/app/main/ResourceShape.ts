d7653ec75bad6bfebfb5e98850527bd96ea0b553
Shenglong Li <shenglong.li@outlook.com>
2016-07-01 11:16:43 -0700

    Implemented auto resize for resource shape

--- src/app/main/ResourceShape.ts
+++ src/app/main/ResourceShape.ts
@@ -2,6 +2,15 @@
 
 module ArmViz {
   export class ResourceShape extends joint.shapes.basic.Generic {
+    protected _minWidth = 100;
+    protected _maxWidth = 200;
+    protected _minHeight = 80;
+    protected _maxHeight = 120;
+    protected _defaultWidth = 120;
+    protected _defaultHeight = 90;
+    protected _width;
+    protected _height;
+
     markup: string = [
       '<g class="rotatable">',
       '<g class="scalable">',
@@ -19,8 +28,13 @@ module ArmViz {
     sourceResource: Resource;
     sourceToolboxItem: ToolboxResource;
 
-    constructor(template: ArmTemplate, resource: Resource, toolboxItem: ToolboxResource) {
-      super();
+    constructor(
+      template: ArmTemplate,
+      resource: Resource,
+      toolboxItem: ToolboxResource,
+      attributes?: any,
+      options?: any) {
+      super(attributes, options);
 
       this.sourceTemplate = template;
       this.sourceResource = resource;
@@ -31,8 +45,73 @@ module ArmViz {
       );
 
       this.initializeContent();
-      this.resize(120, 90);
+      this.resize(this.defaultWidth, this.defaultHeight);
     }
+
+    get minWidth(): number {
+      return this._minWidth;
+    }
+
+    get maxWidth(): number {
+      return this._maxWidth;
+    }
+
+    get minHeight(): number {
+      return this._minHeight;
+    }
+
+    get maxHeight(): number {
+      return this._maxHeight;
+    }
+
+    get defaultWidth(): number {
+      return this._defaultWidth;
+    }
+
+    get defaultHeight(): number {
+      return this._defaultHeight;
+    }
+
+    get width(): number {
+      return this._width;
+    }
+
+    set width(value: number) {
+      if (this.width !== value) {
+        this.resize(value, this.height);
+      }
+    }
+
+    get height(): number {
+      return this._height;
+    }
+
+    set height(value: number) {
+      if (this.height !== value) {
+        this.resize(this.width, value);
+      }
+    }
+
+    resize(width: number, height: number): joint.dia.Element {
+      width = Math.max(this.minWidth, Math.min(this.maxWidth, width));
+      height = Math.max(this.minHeight, Math.min(this.maxHeight, height));
+
+      super.resize(width, height);
+      // console.log(width);
+      // console.log(height);
+
+      this._width = width;
+      this._height = height;
+
+      // Need to set size explicitly for IE and Edge
+      this.attributes.attrs['.shape.root-layout'].width = width;
+      this.attributes.attrs['.shape.root-layout'].height = height;
+      this.attributes.attrs['.shape.title-bar'].width = width;
+
+
+      return this;
+    }
+
 
     private initializeContent() {
       let title = this.sourceToolboxItem.friendlyName;
@@ -43,15 +122,12 @@ module ArmViz {
         'follow-scale': true,
         'stroke': '#0079D6',
         'stroke-width': 2,
-        'width': 120,   // Need to set size explicitly for IE and Edge
-        'height': 90
       };
 
       this.attributes.attrs['.shape.title-bar'] = {
         'follow-scale': true,
         'fill': '#0079D6',
         'stroke-width': 0,
-        'width': 120,
         'height': 15
       };
 
@@ -63,7 +139,8 @@ module ArmViz {
         'stroke-width': 0,
         'font-size': 10,
         'font-weight': 'bold',
-        'text': title
+        'text': title,
+        'id': 'shape-title-' + this.cid
       };
 
       this.attributes.attrs['.shape.icon'] = {
@@ -102,8 +179,32 @@ module ArmViz {
     render(): Backbone.View<joint.dia.Cell> {
       super.render();
 
-      let $text: any = $('#shape-name-' + this.model.cid)[0];
-      console.log($text.getBBox().width);
+      let title: any = $('#shape-title-' + this.model.cid)[0];
+      let name: any = $('#shape-name-' + this.model.cid)[0];
+      let titleWidth = title.getBBox().width + 20;
+      let nameWidth = name.getBBox().width + 20;
+      let model = <ResourceShape>this.model;
+
+      // Resize according to the length of title and name
+      if (titleWidth > model.width) {
+        model.width = titleWidth;
+      }
+      if (nameWidth > model.width) {
+        model.width = nameWidth;
+      }
+
+      // Necessary for IE and Edge
+      super.render();
+      title = $('#shape-title-' + this.model.cid)[0];
+      name = $('#shape-name-' + this.model.cid)[0];
+
+      // Trim text content if it is too long
+      if (titleWidth > model.maxWidth) {
+        title.textContent = name.textContent.substr(0, 37) + '...';
+      }
+      if (nameWidth > model.maxWidth) {
+        name.textContent = name.textContent.substr(0, 37) + '...';
+      }
 
       return this;
     }
